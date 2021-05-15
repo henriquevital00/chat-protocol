93cbfb4b5815b523be0d98c95637f8d883152f62
GabrielBueno200 <gabrielbueno200@gmail.com>
2021-05-15 11:55:46 -0300

    Updated all controllers

--- Services/RoomService.py
+++ Services/RoomService.py
@@ -5,3 +5,51 @@ class RoomService():
 
     roomRepository = RoomRepository()
 
+
+    def findAll(self):
+
+        rooms = list(self.roomRepository.findAll())
+
+        return rooms if len(rooms) > 0 else "No rooms were created!"
+
+
+    def findUsersAtRoom(self, id):
+
+        users = list(self.roomRepository.findUsersAtRoom(id))
+
+        return users if len(users)> 0 else "No users joined this room!"
+
+    def findRoomMessages(self, id):
+
+        users = list(self.roomRepository.findRoomMessages(id))
+
+        return users if len(users)> 0 else "No messages were sent in this room!"
+
+
+    def saveRoom(self, room_name, is_public):
+
+        try:
+            room = { "name": room_name, "is_public": is_public}
+
+            self.roomRepository.saveRoom(room)
+
+            return f"Created room {room} succesfulyy!"
+
+        except:
+
+            return "Not corresponding data was inserted"
+
+
+    def insertUserInRoom(self, room_id, user_id):
+
+        try:
+            room_user = { "room_id": room_id, "user_id": user_id}
+
+            self.roomRepository.insertUserInRoom(room_user)
+
+            return f"User joined succesfulyy!"
+
+        except:
+
+            return "Not corresponding data was inserted"
+
