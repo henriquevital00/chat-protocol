3f390dd8fd26b672f5817f84a9bf1646086e5d2a
GabrielBueno200 <gabrielbueno200@gmail.com>
2021-05-14 23:59:51 -0300

    started rooms repository

--- Repositories/RoomRepository.py
+++ Repositories/RoomRepository.py
@@ -1,8 +1,16 @@
 import peewee
 from Models.User import User
 from Models.Room import Room
+from Models.User import User
+from Models.Room import *
 
 class RoomRepository():
 
     def findAll(self):
-        return Room
+        return Room.select()
+
+    def findUsersAtRoom(self, id):
+        return RoomUser.select(User.id, User.username) \
+            .join(User, on=(User.id == RoomUser.user_id)) \
+            .join(Room, on=(Room.id == RoomUser.room_id)) \
+            .where(Room.id == id)
--- Resources/RoomResources.py
+++ Resources/RoomResources.py
@@ -7,4 +7,10 @@ class UserResources():
     roomService = RoomService()
 
     def findAll(self):
-        pass
+        return Room.select()
+
+    def findUsersAtRoom(self, id):
+        return RoomUser.select(User.id, User.username)\
+               .join(User, on=(User.id == RoomUser.user_id))\
+               .join(Room, on=(Room.id == RoomUser.room_id))\
+               .where(Room.id == id)
