d02fe451a05fa3631bd36af30c04b82527cfc52e
felipepmoreno <felipepmoreno@gmail.com>
2021-05-15 14:07:29 -0300

    Send

--- Commands/Send.py
+++ Commands/Send.py
@@ -3,13 +3,16 @@ class Send:
     def send_message(message):
         pass
 
-    def send_files(file):
+    def send_file(file):
         pass
 
     @staticmethod
     def run(command):
         var = command.split(' ')
         del var[0]
-        if var[0] == '-f':
-            Send.send(var[1])
+        if var[0] == "-f":
+            Send.send_message(var[1])
+        elif var[0] == "-m":
+            Send.send_file(var[1])
         else:
+            pass
