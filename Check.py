c5e7715aa4a1869842f0f2fc4d5da3c6c402b151
henriquevital00 <henriquevital1000@hotmail.com>
2021-05-16 18:49:56 -0300

    Fixed regex

---
+++ Check.py
@@ -0,0 +1,24 @@
+import re
+from Commands.Get import Get
+from Commands.List import List
+from Commands.Send import Send
+from Commands.Create import Create
+from Commands.Accept import Accept
+from Commands.Decline import Decline
+from Commands.Left import Left
+
+
+class Check:
+    @staticmethod
+    def validateCommand(command):
+        pattern = '(^send (-m)?\ .+$)|(^send (-f)?\ \S+\.\S+$)|(^send \S+ (((-m)?\ .+)|((-f)?\ \S+\.\S+))$)|(^get [\w\d\/]+\.\w+$)|(^list files$)|(^list users$)|(^list rooms$)|(^list -r$)|(^accept \S+$)|(^decline \S+$)|(^left( -r)?\ \S+$)|(^create \S+$)|(^create -r \S+$)|(^login \S+ \S+$)|(^logout$)'
+        isValid = re.search(pattern, command)
+        if isValid:
+            string = command.split(' ')[0]
+            string = string.capitalize()
+            eval(string).run(command)
+        else:
+            print('This is not a valid command')
+
+
+Check.validateCommand('list users')
