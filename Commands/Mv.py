7afe5fa2ac2bd1b92323ab656c4453284a883dbf
henriquevital00 <henriquevital1000@hotmail.com>
2021-05-18 22:26:21 -0300

    Implemented move command

---
+++ Commands/Mv.py
@@ -0,0 +1,12 @@
+from Auth.Session import Session
+from Controllers.RoomController import RoomController
+
+
+class Mv:
+    @staticmethod
+    def move(room):
+        Session.get_instance(RoomController().findByName(room)[0])
+
+    @staticmethod
+    def run(command):
+        Mv.move(command.split(' ')[1])
