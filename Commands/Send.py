ba4fa2c130461b840bf7a850b2d4efc59d7d1105
felipepmoreno <felipepmoreno@gmail.com>
2021-05-22 14:03:03 -0300

    Removidas as opções de arquivo

--- Commands/Send.py
+++ Commands/Send.py
@@ -14,15 +14,6 @@ class Send:
     def sendGroupMsg(message, to_user):
         mc.saveMessage(from_user.logged_user(), message)
 
-    def sendPrivateFile(file):
-        
-        #falta implementar para enviar imagens
-        pass
-
-    def sendGroupFile(file):
-        
-        #falta implementar para enviar imagens
-        pass
 
     @staticmethod
     def run(command):
@@ -33,14 +24,12 @@ class Send:
             string += var[i] + " "
         string += var[len(var)-1]
 
-        if var[0] != "-f" or var[0] != "-m": 
+        if var[0] != "-m": 
             to_user = var[0]
             if var[1] == "-m":
                 Send.sendPrivateMsg(string, to_user)
-            if var[1] == "-f":
-                Send.sendPrivateFile(file, to_user)
+           
         else:
             if var[0] == "-m":
                 Send.sendGroupMsg(string, to_user)
-            if var[0] == "-f":
-                Send.sendGroupFile(file, to_user)
+           
