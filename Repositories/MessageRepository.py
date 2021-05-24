a7d405bbaa2829493088afdb4c124088ed5ee499
GabrielBueno200 <gabrielbueno200@gmail.com>
2021-05-23 22:54:46 -0300

    organized repositories

--- Repositories/MessageRepository.py
+++ Repositories/MessageRepository.py
@@ -1,7 +1,7 @@
 from Models.Message import Message
 from Models.User import User
 
-
 class MessageRepository():
+
     def saveMessage(self, message):
         return Message.create(**message)
--- Repositories/UserRepository.py
+++ Repositories/UserRepository.py
@@ -1,26 +1,30 @@
 from Models.User import User
 from Models.Room import RoomUser, Room
 
-
 class UserRepository():
+
     def findAll(self):
         return User.select()
 
     def findByName(self, username):
-        return (User.select().where(User.username == username))
+        return User.select().where(User.username == username)
 
     def findByUsername(self, username):
         return User.select().where(User.username == username)
 
-    def findById(self, id: int):
+    def findById(self, id):
         return User.select().where(User.id == id)
 
     def saveUser(self, user):
         return User.create(**user)
 
-    def findUserRooms(self, id: int):
+    def findUserRooms(self, id):
 
         r = Room.alias()
-        return (RoomUser.select(r.id, r.name, RoomUser.isInRoom).join(
-            r, on=(r.id == RoomUser.room_id),
-            attr="user_room").switch(RoomUser).where(RoomUser.user_id == id))
+
+        return (
+            RoomUser.select(r.id, r.name, RoomUser.isInRoom)
+            .join(r, on=(r.id == RoomUser.room_id),attr="user_room")
+            .switch(RoomUser)
+            .where(RoomUser.user_id == id)
+        )
